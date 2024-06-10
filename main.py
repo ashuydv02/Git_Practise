682ef25b01e4c0963b23591b4e2ad767a3ebe0cf
ashuydv02 <ay545153@gmail.com>
2024-06-10 10:40:33 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-for i in range(1,11):
-		print("5 *",i,"=",i*5)
+for i in range(1, 11):
+    print("5 *", i, "=", i * 5)
+v = 5
+u = 1
+v += u
+while True:
+    print(u)
+    if u == 10:
+        break
+    u += 1
+print(v)
 print("Done")
 print("End of the Program")
 print("New Line")
